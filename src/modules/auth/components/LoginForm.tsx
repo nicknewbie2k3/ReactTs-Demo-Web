a51ef963e68e096403a36a57af2c1aba2ea00b06
nicknewbie2k3 <116050319+nicknewbie2k3@users.noreply.github.com>
2024-09-13 16:37:12 +0700

    Add files via upload

--- src/modules/auth/components/LoginForm.tsx
+++ src/modules/auth/components/LoginForm.tsx
@@ -52,10 +52,24 @@ const LoginForm = (props: Props) => {
             </Box>
         </Box>)
 }
-export default LoginForm */
+export default LoginForm;
+
+*/
+
 import React from 'react';
 import { ILoginParams, ILoginValidation } from "../../../models/auth";
 import { FormattedMessage} from 'react-intl';
+import { validateLogin, validLogin } from '../utils';
+import { ROUTES } from '../../../configs/routes';
+import { useDispatch } from "react-redux";
+import { replace } from 'connected-react-router';
+import { ThunkDispatch } from 'redux-thunk';
+import { AppState } from '../../../redux/reducer';
+import { Action } from 'redux';
+import Cookies from 'js-cookie';
+import { json } from 'stream/consumers';
+import { ACCESS_TOKEN_KEY } from '../../../utils/constants';
+import { Alert } from '@mui/material';
 
 interface Props {
     onLogin(values: ILoginParams): void;
@@ -64,15 +78,42 @@ interface Props {
 }
 
 const LoginForm = (props: Props) => {
+    const dispatch = useDispatch<ThunkDispatch<AppState, null, Action<string>>>();
 
     const { onLogin, loading, errorMessage } = props;
 
     const [formValues, setFormValues] = React.useState<ILoginParams>({email: '', password: '', rememberMe: false});
     const [validate, setValidate] = React.useState<ILoginValidation>();
+    const [remState, setRemState] = React.useState(false);
+
+    const navRegister = () => {
+        dispatch(replace(ROUTES.register));
+    }
+
+    const remMe = () => {
+        setFormValues({...formValues, rememberMe: true});
+    }
 
     const onSubmit = React.useCallback(() => {
+        const validate = validateLogin(formValues);
+        setValidate(validate);
+        if (!validLogin(validate)) {
+            return;
+        }
+        if (formValues.rememberMe){
+            localStorage.setItem(formValues.email, formValues.password);
+        }
+        Cookies.set(ACCESS_TOKEN_KEY, "1", );
         onLogin(formValues);
-    }, [formValues, onLogin]);
+        dispatch(replace(ROUTES.home));
+    }, [dispatch, formValues, onLogin]);
+
+    if (Cookies.get(ACCESS_TOKEN_KEY))
+    {
+        dispatch(replace(ROUTES.home));
+    }
+
+    errorMessage !== "" && <Alert severity="error">{errorMessage}</Alert>
 
     return (
         <form
@@ -85,17 +126,23 @@ const LoginForm = (props: Props) => {
                     <FormattedMessage id="email" />
                 </label>
                 <input type="text" className="form-control" id="inputEmail" value={formValues.email} onChange={(e) => setFormValues({ ...formValues, email: e.target.value})}/>
+
+                {!!validate?.email && (
+                    <small className="text-danger">
+                        <FormattedMessage id={validate?.email} />
+                    </small>
+                )}
             </div>
 
             <div className="col-md-12">
                 <label htmlFor="inputPassword" className="form-label">
                     <FormattedMessage id="password" />
                 </label>
-                <input type="password" className="form-control" id="inputPassword" />
+                <input type="password" className="form-control" id="inputPassword" value={formValues.password} onChange={(e) => setFormValues({ ...formValues, password: e.target.value})}/>
             </div>
 
             <div className="col-12">
-                <div className="form-check">
+                <div className="form-check" onClick={remMe}>
                     <input className="form-check-input" type="checkbox" id="invalidCheck" value="" />
                     <label className="form-check-label" htmlFor="invalidCheck">
                         <FormattedMessage id="rememberMe" />
@@ -108,10 +155,22 @@ const LoginForm = (props: Props) => {
                     <button
                     className="btn btn-primary"
                     type="submit"
+                    onClick={onSubmit}
+                    disabled={loading}
                     style={{minWidth:'160px', display: 'flex', alignItems: 'center', justifyContent: 'center'}}
                     >
-                        <FormattedMessage id="register" />
+                        <FormattedMessage id="login" />
                     </button>
+
+                        <button
+                            className="btn btn-primary"
+                            type="submit"
+                            onClick={navRegister}
+                            style={{minWidth:'160px', display: 'flex', alignItems: 'center', justifyContent: 'center'}}
+                            >
+                            <FormattedMessage id="register" />
+                        </button>
+
                 </div>
             </div>
         </form>
